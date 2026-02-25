ced61b1407da6aa1d4537260e6a57627779e55e0
lindexi <lindexi_gd@163.com>
2026-02-25 19:56:09 +0800

    测试超级多线程同时读写

---
+++ Workbench/YanurlurcaiwulerekeGijurcalgalyearnu/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Concurrent;
+
+ConcurrentDictionary<int, int> dictionary = [];
+
+for (int i = 0; i < 100; i++)
+{
+    Task.Run(() =>
+    {
+        while (true)
+        {
+            var key = Random.Shared.Next(0, 10000);
+            dictionary[key] = Random.Shared.Next();
+        }
+    });
+}
+
+//await Task.Delay(1000);
+Thread.Sleep(TimeSpan.FromSeconds(1));
+
+for (int i = 0; i < 100; i++)
+{
+    var list = dictionary.Values.ToList();
+}
+
+Console.WriteLine("Hello, World!");
