9c905bb94cf694550555af76c7f3fec859cfed35
lindexi <lindexi_gd@163.com>
2026-03-02 19:56:11 +0800

    开放扩展方法，方便业务调用

--- LightTextEditorPlus/LightTextEditorPlus.Avalonia/Utils/TextUnitConverter.cs
+++ LightTextEditorPlus/LightTextEditorPlus.Avalonia/Utils/TextUnitConverter.cs
@@ -3,15 +3,15 @@ using LightTextEditorPlus.Core.Primitive;
 
 namespace LightTextEditorPlus.Utils;
 
-static class TextUnitConverter
+public static class TextUnitConverter
 {
-    public static TextPoint ToTextPoint(this Point point)
+    internal static TextPoint ToTextPoint(this Point point)
         => new(point.X, point.Y);
 
-    public static Point ToPoint(this TextPoint textPoint)
+    public static Point ToAvaloniaPoint(this TextPoint textPoint)
         => new(textPoint.X, textPoint.Y);
 
-    public static TextRect ToTextRect(this Rect rect)
+    internal static TextRect ToTextRect(this Rect rect)
         => TextRect.FromLeftTopRightBottom(rect.Left, rect.Top, rect.Right, rect.Bottom);
 
     public static Rect ToAvaloniaRect(this TextRect textRect)
