511f5e183486113b3c5d645e58ec7abfee820b4d
lindexi <lindexi_gd@outlook.com>
2026-05-06 09:46:20 +0800

    测试错误的情况

--- Microsoft.Agents.AI.Extensions/demo/DeepSeekDemo/Program.cs
+++ Microsoft.Agents.AI.Extensions/demo/DeepSeekDemo/Program.cs
@@ -2,10 +2,21 @@
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.AI.DeepSeek;
 
+using OpenAI;
+
+using System.ClientModel;
+
 var keyFile = @"C:\lindexi\Work\deepseek.txt";
 var key = File.ReadAllText(keyFile);
 
 using var chatClient = new DeepSeekChatClient(key, "deepseek-v4-pro");
+
+// 如果用 OpenAIClient 的话，会出现 HTTP 400 (invalid_request_error: invalid_request_error)： The `reasoning_content` in the thinking mode must be passed back to the API. 错误
+//var openAiClient = new OpenAIClient(new ApiKeyCredential(key), new OpenAIClientOptions()
+//{
+//    Endpoint = new Uri("https://api.deepseek.com/v1")
+//});
+//var openAiChatClient = openAiClient.GetChatClient("deepseek-v4-pro").AsIChatClient();
 
 ChatClientAgent agent = chatClient
     .AsBuilder()
